6223a56fcfda08d78b72249128384f1ac48af80f
novaquantum <37255943+novaquantum@users.noreply.github.com>
2018-08-11 15:26:08 -0700

    Welcome to the world wide web.
    Should be on gitlab tf

---
+++ welcomeParty_github.py
@@ -0,0 +1,18 @@
+print("Welcome")
+print("My name is Satya Nadella")
+print("I bought your code")
+print("What is your name? I already know it")
+name=input("Input to Microsoft Database")
+print("I own your code", name)
+
+
+print("In another life you are Mr.NQ while this life your are submitting your code to github")
+print("Damnt hat other life would be 420 meme approved ")
+print("Be seeing you later", name)
+
+answer = input("Close Program?")
+if answer == "Yes":
+   print("You can take driving lessons to prepare for your driving test.")
+elif answer == "No":
+   print("You are too young to learn how to drive a car.")
+
