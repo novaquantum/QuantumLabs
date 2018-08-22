1d745fd68f832d9dbcf7c4623b46459b68beeae4
novaquantum <37255943+novaquantum@users.noreply.github.com>
2018-08-22 16:26:39 -0700

    - The fixed version of thanos's conversion system
    wtf

---
+++ NQ-SYSTEM-CONVERSION.py
@@ -0,0 +1,8 @@
+print("Welcome to NQ(TM) Conversion System Manager 0.1")
+# Welcome
+print("Input numbers")
+
+x = int(input("Enter a number: "))
+ 
+finalchoice = x * 1.8 + 32 
+print("The conversion is: ", finalchoice)
