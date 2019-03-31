1431420a1c2ac1e9ce8644f155b8bd2a579e751f
novaquantum <37255943+novaquantum@users.noreply.github.com>
2019-03-31 12:29:42 -0700

    #halttmancoworksfacility members
    Welcome to haltman bank where we give your hard earned rips dough!

---
+++ bankofhalttmanv1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python2.7
+# Welcome to Haltmann calculator debugger
+print("Enter bank name") # Prints the bank statement
+name=input() # Name input
+print("Hello")#prints name
+print(name)
+print("Calculating money...") #print
